be8315db87733f9377508cdbf18a9b34a82be6e8
Atanas Vasilev <my.atanas.vasilev@gmail.com>
2024-02-06 10:48:47 +0200

    implement filter functionality

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,10 +14,22 @@ import { HousingService } from '../housing.service';
 export class HomeComponent {
   readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
+  filteredLocationList: HousingLocation[] = [];
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
 
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(cityName: string) {
+    if (!cityName) {
+      return (this.filteredLocationList = this.housingLocationList);
+    }
+
+    return (this.filteredLocationList = this.housingLocationList.filter(
+      (housing) => housing.city.toLowerCase().includes(cityName.toLowerCase())
+    ));
   }
 }
