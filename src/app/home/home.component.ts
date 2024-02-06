23820d246be2ceef249adf666dc8347589c511b3
Atanas Vasilev <my.atanas.vasilev@gmail.com>
2024-02-06 10:25:32 +0200

    Export all templates to standalone html files

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,20 +8,7 @@ import { HousingService } from '../housing.service';
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule, HousingLocationComponent],
-  template: `
-    <section>
-      <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
-      </form>
-    </section>
-    <section class="results">
-      <app-housing-location
-        *ngFor="let housingLocation of housingLocationList"
-        [housingLocation]="housingLocation"
-      ></app-housing-location>
-    </section>
-  `,
+  templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
